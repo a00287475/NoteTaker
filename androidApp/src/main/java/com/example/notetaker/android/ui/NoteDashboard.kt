09c269d81541c7970685c96475718241f6f25a51
a00287475 <a00287475@mycambrian.ca>
2025-02-19 22:48:32 -0500

    organised NoteDashboard

--- androidApp/src/main/java/com/example/notetaker/android/ui/NoteDashboard.kt
+++ androidApp/src/main/java/com/example/notetaker/android/ui/NoteDashboard.kt
@@ -3,6 +3,9 @@ package com.example.notetaker.android.ui
 import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -42,10 +45,19 @@ fun NoteDashboard(navController: NavController) {
         if (notes.value.isEmpty()) {
             Text(text = "No notes yet", style = MaterialTheme.typography.bodyMedium)
         } else {
-            LazyColumn {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2), // 2 columns for a chessboard-like layout
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(8.dp), // Add padding around the grid
+                verticalArrangement = Arrangement.spacedBy(8.dp), // Vertical spacing between items
+                horizontalArrangement = Arrangement.spacedBy(8.dp) // Horizontal spacing between items
+            )
+            {
                 items(notes.value) { note ->
                     NoteItem(note)
                 }
+
+
             }
         }
 
@@ -54,3 +66,5 @@ fun NoteDashboard(navController: NavController) {
 
     }
 }
+
+
---
+++ androidApp/src/main/java/com/example/notetaker/android/ui/TextOverflow.kt
@@ -0,0 +1,5 @@
+package com.example.notetaker.android.ui
+
+class TextOverflow {
+
+}
